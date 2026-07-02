62ca9928b3821ae3441633a7dcd96a1e6a53e7b8
Gabriell77 <161720853+GabrielQR7@users.noreply.github.com>
2026-07-01 22:30:33 -0300

    modal 1/7

--- src/app/Components/modal/Newappointment.jsx
+++ src/app/Components/modal/Newappointment.jsx
@@ -6,17 +6,55 @@ export default function NovoAgendamento({ onClose }) {
   return (
     <div className={styles.overlay}>
       <div className={styles.modal}>
-        <h2>Novo Agendamento</h2>
+        <div className={styles.header}>
+          <h2>Novo Agendamento</h2>
+          <p>Crie seu novo agendamento aqui!</p>
+        </div>
 
-        <input type="text" placeholder="Cliente" />
-        <input type="date" />
-        <input type="time" />
+        <input
+          placeholder="Código"
+          type="text"
+        />
+
+        <select>
+          <option>Cliente</option>
+        </select>
+
+        <select>
+          <option>Vendedor</option>
+        </select>
+
+        <div className={styles.row}>
+          <input type="date" />
+          
+          <input type="time"  />
+        </div>
+
+        <div className={styles.row}>
+          <input
+            placeholder="Cidade"
+            type="text"
+          />
+
+          <input
+            placeholder="Quant_Pessoas"
+            type="number"
+            min={1}
+            max={8}
+          />
+        </div>
+
+        <textarea
+          placeholder="Observações"
+        />
 
         <div className={styles.buttons}>
           <button>Salvar</button>
-          <button onClick={onClose}>Fechar</button>
+          <button onClick={onClose}>Cancelar</button>
         </div>
       </div>
     </div>
   );
 }
+
+//
