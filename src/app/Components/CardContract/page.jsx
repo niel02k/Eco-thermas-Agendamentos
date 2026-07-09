6d1956e45fbcdd172fee487ede2483f9ec05fc6d
fabricio861 <fabriciomansano340@gmail.com>
2026-07-09 12:37:14 -0300

    9/07

--- src/app/Components/CardContract/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"; 
-import styles from './cardContract.module.css'
-
-function CardContract(){
-    return(
-        <div className={styles.contractPage}>
-        </div>
-    )
-}export default CardContract;
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import Login from "@/app/Login/page";
 import Calendar from "@/app/Components/Calendary/Calendary.jsx";
-import CardContrato from "@/app/Components/CardContract/page.jsx"
+
 
 export default function Home() {
   return (
 
 
 
-   <CardContrato></CardContrato>
+   <Login></Login>
 
    
 
