51d9fe98a2242b22bb568f2f04ae9572261a637a
Marcos Daniel <nielfreitasfree@gmail.com>
2026-07-27 23:46:57 -0300

    save

--- src/app/(authenticated)/Reports/page.jsx
+++ src/app/(authenticated)/Reports/page.jsx
@@ -11,7 +11,7 @@ import PageHeader from "@/app/Components/PageHeader/PageHeader.jsx";
 import StatCard from "@/app/Components/Cards/StatCard/StatCard.jsx";
 import styles from "./Reports.module.css";
 import { useContratos } from "@/app/hooks/useContratos";
-import { useAgendamentos } from "@/app/hooks/agendamentos/useAgendamentos";
+import { useAgendamentos } from "@/app/hooks/useAgendamentos";
 import { useConsultores } from "@/app/hooks/useConsultores";
 import { exportToExcel, exportToPDF } from "@/app/services/exportServices.js";
 
